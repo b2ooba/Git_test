e02092032d1295df1d3c8df229457ac58efcfed8
Thagman0 <thagman945@gmail.com>
2023-12-14 09:30:49 +0100

    clientnew

--- client.py
+++ client.py
@@ -1,4 +1,15 @@
-bla bla bla serveugfhjvijoplf
-test
-test
-je
+import threading
+import time
+import socket
+
+class client:
+    def __init__(self, server_ip = "127.0.0.1", port=8999):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect_to_server(server_ip, port)
+        def connect_to_server(self, ip, port):
+            try:
+                print("Connecting to the Server...")
+                self.server_socket.connect((ip,port))
+                print("Connected.")
+            except Exception as e:
+                print("Error Connecting to the Server : ", str(e))
