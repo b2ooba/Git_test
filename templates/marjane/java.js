662a7bf86d5c9e9b24a8b2030c9337a817c9af4f
b2ooba <dialloguelin23@gmail.com>
2024-01-08 22:39:59 +0100

    login

---
+++ templates/marjane/java.js
@@ -0,0 +1,7 @@
+document.getElementById('openPopupImage').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'block';
+});
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
